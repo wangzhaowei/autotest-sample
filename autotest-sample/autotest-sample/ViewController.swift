e374608e4499974b5090589b44fdb71dc3e0c07d
ZhaoweiWang <wangzhaowei1991@gmail.com>
2022-05-02 18:16:37 +0800

    init commit

---
+++ autotest-sample/autotest-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autotest-sample
+//
+//  Created by 王昭威 on 2022/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
